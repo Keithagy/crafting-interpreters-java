39c61efa9e79229bc2be71d99d23bfca05d384a6
Keith Ang <keithang@Keiths-MacBook-Pro.local>
2024-08-01 16:15:22 +0800

    remove boilerplate test

--- app/src/test/java/keithang/craftinginterpreters/AppTest.java
+++ app/src/test/java/keithang/craftinginterpreters/AppTest.java
@@ -1,12 +1,4 @@
 package keithang.craftinginterpreters;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 class AppTest {
-  @Test
-  void appHasAGreeting() {
-    Lox classUnderTest = new Lox();
-    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-  }
 }
