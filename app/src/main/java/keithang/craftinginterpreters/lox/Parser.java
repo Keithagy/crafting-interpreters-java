40f4bf71fb434ebfb2594682b9e5933e9cd4a65e
Keith Ang <keithang@Keiths-MacBook-Pro.local>
2024-08-02 17:18:35 +0800

    add syntax tree parser (single-expression only for now), with order of operations in grammar

---
+++ app/src/main/java/keithang/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,193 @@
+package keithang.craftinginterpreters.lox;
+
+import java.util.List;
+
+/**
+ * A parser really has 2 jobs: give the corresponding syntax tree for a valid
+ * sequence of tokens, else give useful errors about what is invalid about a
+ * sequence of tokens.
+ *
+ * Don't underestimate the second job! If you think about how important syntax
+ * highlighting and autocomplete is... a parser handles incomplete, half-wrong
+ * code all the time.
+ *
+ * A more complex version of the grammar which outlines operator precedence +
+ * associativity, and in doing so disambiguates the grammar.
+ *
+ * expression → equality ;
+ * equality → comparison ( ( "!=" | "==" ) comparison )* ;
+ * comparison → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+ * term → factor ( ( "-" | "+" ) factor )* ;
+ * factor → unary ( ( "/" | "*" ) unary )* ;
+ * unary → ( "!" | "-" ) unary
+ * | primary ;
+ * primary → NUMBER | STRING | "true" | "false" | "nil"
+ * | "(" expression ")" ;
+ *
+ *
+ */
+class Parser {
+  private static class ParseError extends RuntimeException {
+  }
+
+  private final List<Token> tokens;
+  private int current = 0;
+
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError e) {
+      return null;
+    }
+  }
+
+  private Expr expression() {
+    return equality();
+  }
+
+  private Expr equality() {
+    Expr expr = comparison();
+
+    while (match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr comparison() {
+    Expr expr = term();
+    while (match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL)) {
+      Token operator = previous();
+      Expr right = term();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr term() {
+    Expr expr = factor();
+    while (match(TokenType.PLUS, TokenType.MINUS)) {
+      Token operator = previous();
+      Expr right = factor();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr factor() {
+    Expr expr = unary();
+    while (match(TokenType.SLASH, TokenType.STAR)) {
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr unary() {
+    if (match(TokenType.BANG, TokenType.MINUS)) {
+      return new Expr.Unary(previous(), unary());
+    }
+    return primary();
+  }
+
+  private Expr primary() {
+    // not handling { braces } or [ brackets ] yet...
+    if (match(TokenType.LEFT_PAREN)) {
+      Expr expr = expression();
+      consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+      return new Expr.Grouping(expr);
+    }
+    if (match(TokenType.FALSE))
+      return new Expr.Literal(false);
+    if (match(TokenType.TRUE))
+      return new Expr.Literal(true);
+    if (match(TokenType.NIL))
+      return new Expr.Literal(null);
+
+    if (match(TokenType.NUMBER, TokenType.STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
+    throw error(peek(), "Expect expression.");
+  }
+
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private Token consume(TokenType type, String message) {
+    if (check(type)) {
+      return advance();
+    }
+    throw error(peek(), message);
+  }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
+
+  // Alas, we don't get to see this method in action until we implement
+  // statements. Since right now we can only parse a single expression anyway,
+  // it's no big loss.
+  private void synchronize() {
+    advance();
+    while (!isAtEnd()) {
+      if (previous().type == TokenType.SEMICOLON) {
+        return;
+      }
+      switch (peek().type) {
+        case TokenType.CLASS:
+        case TokenType.FUN:
+        case TokenType.VAR:
+        case TokenType.FOR:
+        case TokenType.IF:
+        case TokenType.WHILE:
+        case TokenType.PRINT:
+        case TokenType.RETURN:
+          return;
+        default:
+          advance();
+      }
+    }
+  }
+
+  private boolean check(TokenType type) {
+    if (isAtEnd()) {
+      return false;
+    }
+    return peek().type == type;
+  }
+
+  private Token advance() {
+    if (!isAtEnd()) {
+      current++;
+    }
+    return previous();
+  }
+
+  private boolean isAtEnd() {
+    return peek().type == TokenType.EOF;
+  }
+
+  private Token peek() {
+    return tokens.get(current);
+  }
+
+  private Token previous() {
+    return tokens.get(current - 1);
+  }
+
+}
